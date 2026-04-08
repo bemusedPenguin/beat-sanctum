c6ce9639e5a7bba33857a78a8a94dbd633d5ad13
Scott Davies <sld2688@icloud.com>
2026-04-08 16:10:58 +0100

    initial commit

---
+++ src/main/java/org/bemusedpenguin/beatsanctum/BeatSanctumApplication.java
@@ -0,0 +1,13 @@
+package org.bemusedpenguin.beatsanctum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatSanctumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatSanctumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bemusedpenguin/beatsanctum/BeatSanctumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bemusedpenguin.beatsanctum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatSanctumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
